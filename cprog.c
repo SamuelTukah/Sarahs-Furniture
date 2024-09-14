d0590577d1b16a02f9b999e53ecbcaa94bc85262
SamuelTukah <159536654+SamuelTukah@users.noreply.github.com>
2024-09-14 13:19:17 +0300

    furniture
    furnit

---
+++ cprog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int x;
+short *y;
+void functionX(void);
+void functionX(void)
+{
+    unsigned char i,j;
+    for (i = 0; i < 10; i++)
+    {
+        int k =10;
+        x+=k+j;
+        {
+            *y=x*i;
+        }
+    }
+    
+}
